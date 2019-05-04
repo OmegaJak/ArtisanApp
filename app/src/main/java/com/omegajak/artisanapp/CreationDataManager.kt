5f673aecef383386c6022179c116b5f4e57c205f
OmegaJak <jak.kruger@gmail.com>
2019-05-04 18:10:33 -0500

    Add spells and creations after leveling

--- app/src/main/java/com/omegajak/artisanapp/CreationDataManager.kt
+++ app/src/main/java/com/omegajak/artisanapp/CreationDataManager.kt
@@ -14,7 +14,7 @@ object CreationDataManager {
                 characteristic = "",
                 tier = "1st",
                 typeSpecificData = ArcanismData(arrayListOf("Absorb Elements", "Alarm (ritual)", "Catapult", "Cause Fear", "Charm Person", "Color Spray",
-                        "Chromatic Orb", "Detect Magic (ritual)", "Disguise Self", "Magic Missile", "Shield", "Tenser's Floating Disk (ritual)"))
+                        "Chromatic Orb", "Detect Magic (ritual)", "Disguise Self", "Feather Fall (Monocle)", "Identify (Monocle)", "Magic Missile", "Shield", "Tenser's Floating Disk (ritual)"))
         ))
 
         creationDataCollection.add(CreationData(
@@ -35,8 +35,8 @@ object CreationDataManager {
                 briefDescription = "",
                 characteristic = "",
                 tier = "2nd",
-                typeSpecificData = ArcanismData(arrayListOf("Blink", "Call Lightning", "Clairvoyance", "Enemies Abound", "Fear", "Glyph of Warding", "Leomund's Tiny Hut (ritual)",
-                        "Meld Into Stone (ritual)", "Melf's Minute Meteors", "Sending", "Wall of Sand", "Wall of Water"))
+                typeSpecificData = ArcanismData(arrayListOf("Blink", "Call Lightning", "Clairvoyance", "Counterspell (Monocle)", "Enemies Abound", "Fear", "Fly (Monocle)",
+                        "Glyph of Warding", "Leomund's Tiny Hut (ritual)", "Meld Into Stone (ritual)", "Melf's Minute Meteors", "Sending", "Wall of Sand", "Wall of Water"))
         ))
 
         creationDataCollection.add(CreationData(
@@ -88,6 +88,25 @@ object CreationDataManager {
                 characteristic = "10-foot radius sphere",
                 tier = "1st",
                 typeSpecificData = TypeSpecificData(TypeSpecificData.CreationType.Alchemy)
+        ))
+
+        creationDataCollection.add(CreationData(
+                name = "Thrust Mine",
+                artistryPointCost = 4,
+                fullDescription = "This Tiny device can be safely set down at a position as an action, and then activates whenever a creature approaches to within 10 feet of the thrust mine.\n" +
+                        "There are three different uses for thrust mines, and each have different compatible equipment.\n" +
+                        "Concoction Launcher: A thrust mine can be equipped to an alchemy creation with an area of effect. In this case, the mine automatically launches the creation at the " +
+                        "creature that triggers the mine. The thrust mine is then expended.\n" +
+                        "Arcane Trap: A thrust mine can be equipped to an arcanism creation that stores a spell that causes harm and that targets only one create and has a range of at least 10 " +
+                        "feet or that has an area of effect. When the mine is triggered, the spell in the arcanism creation is cast. If the spell has a target, it targets the creature that triggered " +
+                        "the mine. If the spell affects an area, the area is centered on the triggering creature. If the spell requires concentration, it lasts until the end of its full duration," +
+                        " at which point the thrust mine is expended.\n" +
+                        "Force Snag: If equipped to an arcane focus, the thrust mine fires a single magic missile dart at any creature that activates it. The dart hits automatically and deals 1d4 + 1" +
+                        " force damage. The thrust mine lasts until it is dealt damage that is not psychic or poison, at which point the mine is expended and ceases to function.",
+                briefDescription = "Either a Concoction Launcher, an Arcane Trap, or a Force Snag.",
+                characteristic = "",
+                tier = "2nd",
+                typeSpecificData = TypeSpecificData(TypeSpecificData.CreationType.Artifice)
         ))
     }
 }
